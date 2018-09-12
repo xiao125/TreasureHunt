08dbd76ce0702e996da7b3bba88c9cf513216346
xiao125 <1252241048@qq.com>
2018-09-13 00:47:21 +0800

    AStar自动寻路

---
+++ Assets/Scripts/AStar/AStarDataStructure.cs
@@ -0,0 +1,37 @@
+﻿public class Point
+{
+    public int x, y;
+
+    public Point(int x,int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public new bool Equals(object obj) //C# 方法
+    {
+        return (obj != null) && (obj is Point) && (((Point)obj).x == x && ((Point)obj).y == y);
+    }
+
+}
+
+public class PointData
+{
+    private Point point;
+    public double g, h;
+    public PointData parent; //父节点
+
+    public PointData(Point point,double g,double h,PointData parent)
+    {
+        this.point = point;
+        this.g = g;
+        this.h = h;
+        this.parent = parent;
+    }
+
+    public double F()
+    {
+        return g + h;
+    }
+}
